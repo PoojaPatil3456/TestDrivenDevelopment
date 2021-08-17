9d55d5d273c3b27bfb1553fee994d8c25554716a
PoojaPatil3456 <poojapatil2402@gmail.com>
2021-08-17 23:36:11 +0530

    Test Cases

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,2 +1,19 @@
 public class Rectangle {
+    public long calculateArea(int length,int width){
+        if(length<=0 || width<=0){
+           // return -1;
+            throw new IllegalArgumentException("Length or width of rectangle should not be zero or less than zero");
+        }
+        else{
+            return (length*width);
+        }
+    }
+    public long calculatePerimeter(int length,int width){
+        if(length<=0 || width<=0){
+            throw new IllegalArgumentException("Length or width of rectangle should not be zero or less than zero");
+        }
+        else{
+            return (2*(length+width));
+        }
+    }
 }
--- src/test/java/RectangleOperations.java
+++ src/test/java/RectangleOperations.java
@@ -1,4 +1,27 @@
-public class RectangleOperations {
-    Rectangle rectangle=new Rectangle();
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class RectangleOperations {
+    Rectangle rectangle;
+
+    @BeforeEach
+    public void createObject(){ rectangle = new Rectangle();}
+
+    @Test
+    void toGetAreaFiftyForLengthTenAndWidthFive(){
+        assertEquals(50,rectangle.calculateArea(10,5));
+    }
+    @Test
+    void toGetPerimeterThirtyForLengthTenAndWidthFive(){
+        assertEquals(30,rectangle.calculatePerimeter(10,5));
+    }
+    @Test
+    void toGetExceptionWhenNegativeValuePassedToArea(){
+        assertEquals(-1,rectangle.calculateArea(-10,5));
+    }
+    @Test
+    void toGetExceptionWhenNegativeValuePassedToPerimeter(){
+        assertEquals(-1,rectangle.calculatePerimeter(-10,5));
+    }
 }
